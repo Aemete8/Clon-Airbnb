76fe59f7b99f91bcd5889789ccc9870ae2392927
Alejo Martinez <brayanm531@gmail.com>
2026-09-23 22:52:31 -0500

    Refactor Header import in MainLayout to default export for consistency with other components.

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router";
-import { Header } from "../layout/Header";
+import Header from "../layout/Header";
 import { Footer } from "../layout/Footer";
 
 export function MainLayout() {
